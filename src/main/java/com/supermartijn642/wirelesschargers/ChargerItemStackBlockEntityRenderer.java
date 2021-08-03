1672c989d1ca435e0e01370c385010e303cb542b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-08-03 17:05:02 +0200

    Fix server crash

--- src/main/java/com/supermartijn642/wirelesschargers/ChargerItemStackBlockEntityRenderer.java
+++ src/main/java/com/supermartijn642/wirelesschargers/ChargerItemStackBlockEntityRenderer.java
@@ -22,6 +22,10 @@ public class ChargerItemStackBlockEntityRenderer extends ItemStackTileEntityRend
 
     public static final ChargerItemStackBlockEntityRenderer INSTANCE = new ChargerItemStackBlockEntityRenderer();
 
+    public static ChargerItemStackBlockEntityRenderer getInstance(){
+        return INSTANCE;
+    }
+
     private ChargerItemStackBlockEntityRenderer(){
     }
 
