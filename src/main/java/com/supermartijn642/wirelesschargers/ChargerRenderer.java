017e0bcbce0a2343ff47aabbc0c7b20398876100
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-11-13 20:12:47 +0100

    Improve the charger area highlighting

--- src/main/java/com/supermartijn642/wirelesschargers/ChargerRenderer.java
+++ src/main/java/com/supermartijn642/wirelesschargers/ChargerRenderer.java
@@ -1,14 +1,14 @@
 package com.supermartijn642.wirelesschargers;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.supermartijn642.core.ClientUtils;
-import net.minecraft.client.renderer.*;
+import com.supermartijn642.core.render.RenderUtils;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Quaternion;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.client.model.data.EmptyModelData;
 
@@ -18,8 +18,6 @@ import java.util.Random;
  * Created 7/9/2021 by SuperMartijn642
  */
 public class ChargerRenderer extends TileEntityRenderer<ChargerBlockEntity> {
-
-    public static final RenderType AREA_HIGHLIGHT_RENDER_TYPE = RenderType.create("wireless_charger_area_highlight", DefaultVertexFormats.POSITION_COLOR, 7, 256, false, true, RenderTypeExtension.getAreaHighlightCompositeState());
 
     public ChargerRenderer(TileEntityRendererDispatcher rendererDispatcher){
         super(rendererDispatcher);
@@ -46,76 +44,18 @@ public class ChargerRenderer extends TileEntityRenderer<ChargerBlockEntity> {
 
         matrixStack.popPose();
 
-        if(entity.isAreaHighlighted())
-            renderAreaHighlight(entity, matrixStack, bufferSource);
-    }
+        if(entity.isAreaHighlighted()){
+            AxisAlignedBB area = entity.getOperatingArea().move(-entity.getBlockPos().getX(), -entity.getBlockPos().getY(), -entity.getBlockPos().getZ()).inflate(0.05);
 
-    public static void renderAreaHighlight(ChargerBlockEntity entity, MatrixStack matrixStack, IRenderTypeBuffer bufferSource){
-        AxisAlignedBB area = entity.getOperatingArea().move(-entity.getBlockPos().getX(), -entity.getBlockPos().getY(), -entity.getBlockPos().getZ()).inflate(0.05);
+            Random random = new Random(entity.getBlockPos().hashCode());
 
-        Matrix4f matrix = matrixStack.last().pose();
-        IVertexBuilder vertexConsumer = bufferSource.getBuffer(AREA_HIGHLIGHT_RENDER_TYPE);
+            float red = random.nextFloat();
+            float green = random.nextFloat();
+            float blue = random.nextFloat();
+            float alpha = 0.2f;
 
-        float minX = (float)area.minX, maxX = (float)area.maxX;
-        float minY = (float)area.minY, maxY = (float)area.maxY;
-        float minZ = (float)area.minZ, maxZ = (float)area.maxZ;
-
-        Random random = new Random(entity.getBlockPos().hashCode());
-
-        float red = random.nextFloat();
-        float green = random.nextFloat();
-        float blue = random.nextFloat();
-        float alpha = 0.2f;
-
-        vertexConsumer.vertex(matrix, minX, minY, minZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
-
-        vertexConsumer.vertex(matrix, minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-
-
-        vertexConsumer.vertex(matrix, minX, minY, minZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-
-        vertexConsumer.vertex(matrix, minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-
-
-        vertexConsumer.vertex(matrix, minX, minY, minZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-
-        vertexConsumer.vertex(matrix, maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-        vertexConsumer.vertex(matrix, maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-    }
-
-    // Just need this to access protected fields
-    private static class RenderTypeExtension extends RenderType {
-
-        public RenderTypeExtension(String p_i225992_1_, VertexFormat p_i225992_2_, int p_i225992_3_, int p_i225992_4_, boolean p_i225992_5_, boolean p_i225992_6_, Runnable p_i225992_7_, Runnable p_i225992_8_){
-            super(p_i225992_1_, p_i225992_2_, p_i225992_3_, p_i225992_4_, p_i225992_5_, p_i225992_6_, p_i225992_7_, p_i225992_8_);
-        }
-
-        public static RenderType.State getAreaHighlightCompositeState(){
-            return RenderType.State.builder()
-                .setAlphaState(DEFAULT_ALPHA)
-                .setTransparencyState(RenderState.TRANSLUCENT_TRANSPARENCY)
-                .setTextureState(NO_TEXTURE)
-                .setCullState(NO_CULL)
-                .setDepthTestState(RenderState.LEQUAL_DEPTH_TEST)
-                .setWriteMaskState(COLOR_WRITE)
-                .createCompositeState(false);
+            RenderUtils.renderBox(matrixStack, area, red, green, blue);
+            RenderUtils.renderBoxSides(matrixStack, area, red, green, blue, alpha);
         }
     }
 }
