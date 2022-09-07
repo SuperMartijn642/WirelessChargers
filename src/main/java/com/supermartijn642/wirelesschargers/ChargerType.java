cbaa1e3ea34edbc64ea3706f5bc3a709c764e924
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-09-07 15:35:06 +0200

    Use BaseBlockEntityType and BaseBlockItem instead of vanilla classes

--- src/main/java/com/supermartijn642/wirelesschargers/ChargerType.java
+++ src/main/java/com/supermartijn642/wirelesschargers/ChargerType.java
@@ -1,5 +1,6 @@
 package com.supermartijn642.wirelesschargers;
 
+import com.supermartijn642.core.block.BaseBlockEntityType;
 import com.supermartijn642.core.item.BaseBlockItem;
 import com.supermartijn642.core.item.ItemProperties;
 import com.supermartijn642.core.registry.RegistrationHandler;
@@ -23,9 +24,9 @@ public enum ChargerType {
     BASIC_WIRELESS_PLAYER_CHARGER(false, true, WirelessChargersConfig.basicPlayerChargerRange, WirelessChargersConfig.basicPlayerChargerCapacity, WirelessChargersConfig.basicPlayerChargerTransferRate, ChargerModelType.BASIC_WIRELESS_PLAYER_CHARGER, "Basic Wireless Player Charger"),
     ADVANCED_WIRELESS_PLAYER_CHARGER(false, true, WirelessChargersConfig.advancedPlayerChargerRange, WirelessChargersConfig.advancedPlayerChargerCapacity, WirelessChargersConfig.advancedPlayerChargerTransferRate, ChargerModelType.ADVANCED_WIRELESS_PLAYER_CHARGER, "Advanced Wireless Player Charger");
 
-    private BlockEntityType<ChargerBlockEntity> blockEntityType;
+    private BaseBlockEntityType<ChargerBlockEntity> blockEntityType;
     private ChargerBlock block;
-    private BlockItem item;
+    private BaseBlockItem item;
     public final boolean canChargeBlocks, canChargePlayers;
     public final Supplier<Integer> range, capacity, transferRate;
     public final ChargerModelType modelType;
@@ -75,7 +76,7 @@ public enum ChargerType {
         if(this.block == null)
             throw new IllegalStateException("Blocks must be registered before registering block entity types!");
 
-        this.blockEntityType = BlockEntityType.Builder.of(this::createBlockEntity, this.block).build(null);
+        this.blockEntityType = BaseBlockEntityType.create(this::createBlockEntity, this.block);
         helper.register(this.getRegistryName() + "_block_entity", this.blockEntityType);
     }
 
