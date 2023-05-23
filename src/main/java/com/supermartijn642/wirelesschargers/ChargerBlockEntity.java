c441b627dbf94458d913dc793d336f8ad8996133
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-05-23 16:27:21 +0200

    Fix energy transfers

--- src/main/java/com/supermartijn642/wirelesschargers/ChargerBlockEntity.java
+++ src/main/java/com/supermartijn642/wirelesschargers/ChargerBlockEntity.java
@@ -114,6 +114,7 @@ public class ChargerBlockEntity extends BaseBlockEntity implements TickableBlock
                                 spawnParticles = true;
                                 this.energy -= transferred;
                                 this.dataChanged();
+                                transaction.commit();
                                 if(this.energy <= 0)
                                     break;
                             }
@@ -146,6 +147,7 @@ public class ChargerBlockEntity extends BaseBlockEntity implements TickableBlock
                                         spawnParticles = true;
                                         this.energy -= transferred;
                                         this.dataChanged();
+                                        transaction.commit();
                                         if(this.energy <= 0)
                                             break loop;
                                         toTransfer -= transferred;
@@ -168,10 +170,10 @@ public class ChargerBlockEntity extends BaseBlockEntity implements TickableBlock
                             try(Transaction transaction = Transaction.openOuter()){
                                 int transferred = (int)storage.insert(toTransfer, transaction);
                                 if(transferred > 0){
-                                    inventory.setItem(i, stack);
                                     spawnParticles = true;
                                     this.energy -= transferred;
                                     this.dataChanged();
+                                    transaction.commit();
                                     if(this.energy <= 0)
                                         break loop;
                                     toTransfer -= transferred;
