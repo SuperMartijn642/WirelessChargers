9af820d818285376fe6ccaf540a68475eb392471
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-07 19:57:00 +0200

    Update to Minecraft 26.1

--- src/main/java/com/supermartijn642/wirelesschargers/ChargerSpecialModelRenderer.java
+++ src/main/java/com/supermartijn642/wirelesschargers/ChargerSpecialModelRenderer.java
@@ -6,27 +6,33 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.supermartijn642.core.ClientUtils;
 import com.supermartijn642.core.CommonUtils;
 import com.supermartijn642.core.block.BaseBlock;
+import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.block.model.BlockModel;
+import net.minecraft.client.renderer.block.model.BlockStateModelWrapper;
+import net.minecraft.client.renderer.block.model.CompositeBlockModel;
+import net.minecraft.client.renderer.block.model.SpecialBlockModelWrapper;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
 import net.minecraft.client.renderer.special.SpecialModelRenderer;
-import net.minecraft.client.renderer.state.CameraRenderState;
+import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.ProblemReporter;
-import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.TagValueInput;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3fc;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 
 /**
  * Created 26/12/2024 by SuperMartijn642
  */
-public class ChargerSpecialModelRenderer implements SpecialModelRenderer.Unbaked {
+public class ChargerSpecialModelRenderer implements SpecialModelRenderer.Unbaked<BlockEntityRenderState> {
 
     public static final MapCodec<ChargerSpecialModelRenderer> CODEC = RecordCodecBuilder.mapCodec(
         instance -> instance.group(
@@ -34,6 +40,14 @@ public class ChargerSpecialModelRenderer implements SpecialModelRenderer.Unbaked
         ).apply(instance, ChargerSpecialModelRenderer::new)
     );
     private static final CameraRenderState DUMMY_CAMERA_RENDER_STATE = new CameraRenderState();
+
+    public static BlockModel.Unbaked createBlockModel(ChargerType type, BlockState state, BlockColors blockColors){
+        return new CompositeBlockModel.Unbaked(
+            new BlockStateModelWrapper.Unbaked(state, blockColors.getTintSources(state), Optional.empty()),
+            new SpecialBlockModelWrapper.Unbaked<>(new ChargerSpecialModelRenderer(type), Optional.empty()),
+            Optional.empty()
+        );
+    }
 
     private final ChargerType chargerType;
     private ChargerBlockEntity entity;
@@ -43,10 +57,10 @@ public class ChargerSpecialModelRenderer implements SpecialModelRenderer.Unbaked
     }
 
     @Override
-    public @Nullable SpecialModelRenderer<?> bake(SpecialModelRenderer.BakingContext context){
-        return new SpecialModelRenderer<BlockEntityRenderState>() {
+    public @Nullable SpecialModelRenderer<BlockEntityRenderState> bake(SpecialModelRenderer.BakingContext context){
+        return new SpecialModelRenderer<>() {
             @Override
-            public void submit(@Nullable BlockEntityRenderState entityRenderState, ItemDisplayContext displayContext, PoseStack poseStack, SubmitNodeCollector output, int combinedLight, int combinedOverlay, boolean hasFoil, int k){
+            public void submit(@Nullable BlockEntityRenderState entityRenderState, PoseStack poseStack, SubmitNodeCollector output, int combinedLight, int combinedOverlay, boolean hasFoil, int k){
                 if(entityRenderState != null)
                     ClientUtils.getMinecraft().getBlockEntityRenderDispatcher().submit(entityRenderState, poseStack, output, DUMMY_CAMERA_RENDER_STATE);
             }
@@ -79,7 +93,7 @@ public class ChargerSpecialModelRenderer implements SpecialModelRenderer.Unbaked
     }
 
     @Override
-    public MapCodec<? extends SpecialModelRenderer.Unbaked> type(){
+    public MapCodec<? extends SpecialModelRenderer.Unbaked<BlockEntityRenderState>> type(){
         return CODEC;
     }
 }
